f77641bcb8ca16d06c13a67b94debf99ab12145f
estefaniazapata255-cyber <estefania.zapata255@pascualbravo.edu.co>
2026-05-04 13:07:53 -0500

    primer commit

---
+++ Empleado.cs
@@ -0,0 +1,32 @@
+public class Empleado : Persona // Esta heredado de la clase padre Persona 
+{
+    private double sueldo;
+    public string cargo;
+    public double Sueldo
+    {
+        get {return sueldo;}
+        set{
+            if(value >= 0)
+            {
+                sueldo = value;
+            }else{
+                Console.WriteLine("Error... El sueldo debe de ser positivo");
+            }
+        }
+    }
+
+    public Empleado // Constructor de empleado 
+    (
+        string nombre,
+        string documento,
+        string correo,
+        int edad,
+        double sueldo,
+        string cargo
+    )
+    :base(nombre, documento, correo, edad); // Le decimos que atributos hereda
+    {
+        this.sueldo = sueldo;
+        this.cargo = cargo;
+    }
+}
